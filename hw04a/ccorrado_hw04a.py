7c7cdf84101579d020d96e60106bf54d89d61053
Christopher Corrado <ccorrad@its.jnj.com>
2018-09-23 18:29:10 -0400

    Add a few more tests.

--- hw04a/ccorrado_hw04a.py
+++ hw04a/ccorrado_hw04a.py
@@ -33,6 +33,8 @@ def get_repos_for_user(github_id):
 
 
 def get_commits_for_repo(repo):
+    if repo == {}:
+        return []
     request = requests.get(BASE_URL + "repos/" + repo.get("owner").get("login") + "/" + repo.get("name") + "/commits")
     if request.status_code == 200:
         return json.loads(request.text)
@@ -40,15 +42,19 @@ def get_commits_for_repo(repo):
         return []
 
 
-def print_repo_results(git_repo_list):
-    for repo in git_repo_list:
-        print(repo)
-
-
-def get_all_repos_and_commits(github_id):
+def get_git_repo_list(github_id):
     git_repo_list = []
     repos = get_repos_for_user(github_id)
     for repo in repos:
         git_repo_list.append(GitRepo(repo, get_commits_for_repo(repo)))
     print_repo_results(git_repo_list)
     return git_repo_list
+
+
+def print_repo_results(git_repo_list):
+    """
+    Method to print {@link GitRepo} Objects
+    :param git_repo_list: List containing GitRepo objects
+    """
+    for repo in git_repo_list:
+        print(repo)
--- tests/test_ccorrado_hw04a.py
+++ tests/test_ccorrado_hw04a.py
@@ -2,7 +2,7 @@ import json
 import unittest
 from unittest import TestCase
 
-from hw04a.ccorrado_hw04a import get_repos_for_user, get_commits_for_repo, GitRepo
+from hw04a.ccorrado_hw04a import get_repos_for_user, get_commits_for_repo, GitRepo, print_repo_results
 
 
 class GitRepoTests(TestCase):
@@ -15,9 +15,26 @@ class GitRepoTests(TestCase):
         Assert that neither list is empty.
         """
         repos = get_repos_for_user("CCorrado")
-        commits = get_commits_for_repo(repos[0])
-        self.assertTrue(repos != [])
-        self.assertTrue(commits != [])
+        git_repos = []
+        git_repo = GitRepo(repos[0], get_commits_for_repo(repos[0]))
+        git_repo2 = GitRepo(repos[1], get_commits_for_repo(repos[1]))
+        git_repo3 = GitRepo(repos[2], get_commits_for_repo(repos[2]))
+        git_repos.append(git_repo)
+        git_repos.append(git_repo2)
+        git_repos.append(git_repo3)
+        self.assertTrue(git_repo.repo != [])
+        self.assertTrue(git_repo.commit_list != [])
+        print_repo_results(git_repos)
+
+    def test_no_data(self):
+        """
+        Assert that the methods return empty lists correctly.
+        """
+        repos = get_repos_for_user("")
+        commits = get_commits_for_repo({})
+        print_repo_results([])
+        self.assertTrue(repos == [])
+        self.assertTrue(commits == [])
 
     def test_local_data(self):
         """
@@ -28,10 +45,10 @@ class GitRepoTests(TestCase):
         Assert that the name is equal to the name of the first repo in the sample file.
         Assert that the length of the commit list is 23.
         """
-        with open("./hw04a/sample_repo_data.json") as file:
+        with open("../hw04a/sample_repo_data.json") as file:
             repos = json.load(file)
             self.assertTrue(repos != [])
-            with open("./hw04a/sample_commit_data.json") as commit_file:
+            with open("../hw04a/sample_commit_data.json") as commit_file:
                 commits = json.load(commit_file)
                 self.assertTrue(repos[0].get("owner").get("node_id") == commits[0].get("author").get("node_id"))
                 git_repo = GitRepo(repos[0], commits)
