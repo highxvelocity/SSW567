f25ad103dc9b9b7b011aef1e07dae42cbc423d47
Christopher Corrado <ccorrad@its.jnj.com>
2018-09-29 11:11:47 -0400

    Adding a validity check and extra unit test.

--- hw01/ccorrado_hw01_triangle.py
+++ hw01/ccorrado_hw01_triangle.py
@@ -2,6 +2,8 @@
 HW01: Triangle Classification
 Author: Chris Corrado
 Date: 8/29/2018
+
+Modified for HW05: 9/29/2018
 """
 
 
@@ -19,28 +21,52 @@ class Triangle(object):
         self.b = b
         self.c = c
 
+    def is_valid(self) -> bool:
+        """
+        Method to check to see if the triangle is a valid one.
+        :return: True if the triangle is valid
+        """
+        return self._is_ints() and self._is_positive() and self._is_triangle()
+
+    def _is_triangle(self) -> bool:
+        """
+        The sum of two sides must be strictly greater than the third side.
+        :return: True if the triangle is a triangle
+        """
+        return ((self.b + self.c) > self.a) or ((self.a + self.c) > self.b) or ((self.a + self.b) > self.c)
+
+    def _is_positive(self) -> bool:
+        return self.a > 0 and self.b > 0 and self.c > 0
+
+    def _is_ints(self) -> bool:
+        return isinstance(self.a, int) and isinstance(self.b, int) and isinstance(self.c, int)
+
     def is_right(self) -> bool:
-        return self.a ^ 2 + self.b ^ 2 == self.c ^ 2
+        return self.is_valid() and self.a ^ 2 + self.b ^ 2 == self.c ^ 2
 
     def is_equilateral(self) -> bool:
-        return self.a == self.b == self.c
+        return self.is_valid() and self.a == self.b == self.c
 
     def is_isosceles(self) -> bool:
-        return (not self.is_equilateral()) and (self.a == self.b or self.a == self.c or self.b == self.c)
+        return self.is_valid() and (not self.is_equilateral()) and (
+                self.a == self.b or self.a == self.c or self.b == self.c)
 
     def is_scalene(self) -> bool:
-        return self.a != self.b != self.c
+        return self.is_valid() and self.a != self.b != self.c
 
     def __str__(self):
         text_to_print = []
-        if self.is_right():
-            text_to_print.append("Right")
-        if self.is_equilateral():
-            text_to_print.append("Equilateral Triangle")
-        elif self.is_isosceles():
-            text_to_print.append("Isosceles Triangle")
-        elif self.is_scalene():
-            text_to_print.append("Scalene Triangle")
+        if not self.is_valid():
+            text_to_print = "Invalid"
+        else:
+            if self.is_right():
+                text_to_print.append("Right")
+            if self.is_equilateral():
+                text_to_print.append("Equilateral Triangle")
+            elif self.is_isosceles():
+                text_to_print.append("Isosceles Triangle")
+            elif self.is_scalene():
+                text_to_print.append("Scalene Triangle")
 
         return str(text_to_print)
 
