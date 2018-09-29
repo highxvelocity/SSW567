c93d17ae5db53c7a2376db14594d42766b0c17aa
Christopher Corrado <ccorrad@its.jnj.com>
2018-09-29 12:41:34 -0400

    Configuring Mocks for HW05a. Updated Readme.

--- tests/test_ccorrado_hw04a.py
+++ tests/test_ccorrado_hw04a.py
@@ -1,40 +1,51 @@
 import json
 import unittest
 from unittest import TestCase
+from unittest.mock import patch
 
-from hw04a.ccorrado_hw04a import get_repos_for_user, get_commits_for_repo, GitRepo, print_repo_results
+from hw04a.ccorrado_hw04a import GitRepo, print_repo_results
 
 
-class GitRepoTests(TestCase):
+class GitRepoTests(TestCase, object):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_get_repo_patch = patch('hw04a.ccorrado_hw04a.get_repos_for_user')
+        cls.mock_get_repo = cls.mock_get_repo_patch.start()
+
+        with open("./hw04a/sample_repo_data.json") as file:
+            cls.mock_get_repo.return_value = json.load(file)
+
+        cls.mock_get_commits_patch = patch('hw04a.ccorrado_hw04a.get_commits_for_repo')
+        cls.mock_get_commits = cls.mock_get_commits_patch.start()
+
+        with open("./hw04a/sample_commit_data.json") as commit_file:
+            cls.mock_get_commits.return_value = json.load(commit_file)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mock_get_commits_patch.stop()
+        cls.mock_get_repo_patch.stop()
 
     def test_online_data(self):
         """
-        This unit test will make 2 network requests
+        This unit test will make 4 network requests
+        Modification, Methods that depend on the network will be mocked.
+
         1. Get Repos for User "CCorrado"
-        2. Get Commits for the first Repository in the list\
+        2. Get Commits for the first Repository in the list
         Assert that neither list is empty.
         """
-        repos = get_repos_for_user("CCorrado")
+        repos = self.mock_get_repo("CCorrado")
+        self.assertTrue(repos != [])
+
         git_repos = []
-        git_repo = GitRepo(repos[0], get_commits_for_repo(repos[0]))
-        git_repo2 = GitRepo(repos[1], get_commits_for_repo(repos[1]))
-        git_repo3 = GitRepo(repos[2], get_commits_for_repo(repos[2]))
+        git_repo = GitRepo(repos[0], self.mock_get_commits(repos[0]))
         git_repos.append(git_repo)
-        git_repos.append(git_repo2)
-        git_repos.append(git_repo3)
         self.assertTrue(git_repo.repo != [])
         self.assertTrue(git_repo.commit_list != [])
-        print_repo_results(git_repos)
 
-    def test_no_data(self):
-        """
-        Assert that the methods return empty lists correctly.
-        """
-        repos = get_repos_for_user("")
-        commits = get_commits_for_repo({})
-        print_repo_results([])
-        self.assertTrue(repos == [])
-        self.assertTrue(commits == [])
+        print_repo_results(git_repos)
 
     def test_local_data(self):
         """
@@ -45,16 +56,17 @@ class GitRepoTests(TestCase):
         Assert that the name is equal to the name of the first repo in the sample file.
         Assert that the length of the commit list is 23.
         """
-        with open("./hw04a/sample_repo_data.json") as file:
-            repos = json.load(file)
-            self.assertTrue(repos != [])
-            with open("./hw04a/sample_commit_data.json") as commit_file:
-                commits = json.load(commit_file)
-                self.assertTrue(repos[0].get("owner").get("node_id") == commits[0].get("author").get("node_id"))
-                git_repo = GitRepo(repos[0], commits)
-                print(git_repo)
-                self.assertEqual("android-RepeatingAlarm", git_repo.repo.get('name'))
-                self.assertEqual(23, len(git_repo.commit_list))
+        repos = self.mock_get_repo("CCorrado")
+        self.assertTrue(repos != [])
+
+        commits = self.mock_get_commits(repos[0])
+        self.assertTrue(repos[0].get("owner").get("node_id") == commits[0].get("author").get("node_id"))
+
+        git_repo = GitRepo(repos[0], commits)
+        print(git_repo)
+
+        self.assertEqual("android-RepeatingAlarm", git_repo.repo.get('name'))
+        self.assertEqual(23, len(git_repo.commit_list))
 
 
 if __name__ == "__main__":
